14811c19abdfb99901eb815d0d5589937e6cb1d7
theSLWayne <hiranhasanka@gmail.com>
2022-05-25 15:11:11 +0530

    Export database records to csv

---
+++ db_to_csv.py
@@ -0,0 +1,22 @@
+"""
+Script to output customer details from the database column
+Data is exported to a csv file for the ease of using it on the clustering model
+"""
+
+import pandas as pd
+from pymongo import MongoClient
+from keys import detailsDict
+
+csv_file = 'data.csv'
+
+client = MongoClient("mongodb://127.0.0.1:27017/")
+db = client[detailsDict.db.DATABASE]
+col = db[detailsDict.db.COLLECTION]
+
+all_tweets = list(col.find())
+print(len(all_tweets))
+print(all_tweets[0])
+
+df = pd.DataFrame(all_tweets)
+df.drop(columns="_id")
+df.to_csv(csv_file, index=False)
